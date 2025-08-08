4172e01103840dd63cff8cd596b5e9db2e27d731
Aaron Ruan <i@ar212.com>
2025-08-08 13:26:18 +0800

    Initial Commit

---
+++ CustomWindowBackgroundDemo/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CustomWindowBackgroundDemo
+//
+//  Created by Aaron212 on 2025-08-08.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
